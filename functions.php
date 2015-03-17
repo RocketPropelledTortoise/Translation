0496facfa869e6d7f6a6fe1f451c1960b51062d3
Stéphane Goetz <onigoetz@onigoetz.ch>
2015-03-17 22:40:21 +0100

    [Translation] Simplify translation helpers

--- functions.php
+++ functions.php
@@ -15,8 +15,12 @@
  * @param string $language
  * @return string
  */
-function __($string, array $args = [], $context = 'default', $language = 'default')
+function t($string, array $args = [], $context = null, $language = 'default')
 {
+    if ($context == null) {
+        $context = I18N::getContext();
+    }
+
     //get translation
     $translated = I18N::translate($string, $context, $language);
 
@@ -46,75 +50,20 @@ function __($string, array $args = [], $context = 'default', $language = 'defaul
 }
 
 /**
- * Translates a string and returns it
- *
- * @param string $string
- * @param array $args
- * @return string
- */
-function t($string, Array $args = [])
-{
-    return __($string, $args, I18N::getContext());
-}
-
-/**
- * Translates and echoes a string to convert
- *
- * @param string $string
- * @param array $args
- * @param string $context
- * @param string $language
- */
-function _e($string, Array $args = [], $context = 'default', $language = 'default')
-{
-    echo __($string, $args, $context, $language);
-}
-
-/**
  * Translates with the correct (singular or plural) form
  *
  * @param string $string
  * @param string $plural
- * @param integer $count
  * @param array $args
  * @param string $context
  * @param string $language
  * @return string
  */
-function _n($string, $plural, $count, Array $args = [], $context = 'default', $language = 'default')
+function tn($string, $plural, array $args = [], $context = 'default', $language = 'default')
 {
-    if ($count <= 1) {
-        return __($string, $args, $context, $language);
+    if ($args['#'] <= 1) {
+        return t($string, $args, $context, $language);
     } else {
-        return __($plural, $args, $context, $language);
+        return t($plural, $args, $context, $language);
     }
-}
-
-/**
- * Translates with the correct (singular or plural) form
- *
- * @param string $string
- * @param string $plural
- * @param int $count
- * @param array $args
- * @param string $language
- * @return string
- */
-function tn($string, $plural, $count, Array $args = [], $language = 'default')
-{
-    return _n($string, $plural, $count, $args, I18N::getContext(), $language);
-}
-
-/**
- * Translates and echoes with the correct (singular or plural) form
- * @param string $string
- * @param string $plural
- * @param integer $count
- * @param array $args
- * @param string $context
- * @param string $language
- */
-function _en($string, $plural, $count, Array $args = [], $context = 'default', $language = 'default')
-{
-    echo _n($string, $plural, $count, $args, $context, $language);
 }
